8ddda78560195882fba3577fbd4aa524a6211ab9
Andrea Zucchini <azga84@gmail.com>
2018-02-06 18:38:20 +0100

    creation and deletion of temporary folder in each test

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,19 @@
 'use strict'
 
 const test = require('ava')
+const os = require('os')
+const path = require('path')
+
+const { createDirectory, emptyDirectory, removeDirectory } = require('./helpers/utils')
+
+const TEST_DIRECTORY = path.join(os.tmpdir(), 'fs.copy')
+test.before(async () => {
+  await emptyDirectory(TEST_DIRECTORY)
+  await createDirectory(TEST_DIRECTORY)
+})
+
+test.after(async () => {
+  await removeDirectory(TEST_DIRECTORY)
+})
+
 test('should pass', t => t.pass())
