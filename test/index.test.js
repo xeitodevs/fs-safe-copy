0187478ef07d108ea9f2c507b60b6d55a10751dc
Andrea Zucchini <azga84@gmail.com>
2018-02-07 21:00:10 +0100

    check if return and error if source and destination are the same

--- test/index.test.js
+++ test/index.test.js
@@ -3,12 +3,14 @@
 const test = require('ava')
 const os = require('os')
 const path = require('path')
+const { copy } = require('../')
 
 const { createDirectory, emptyDirectory, removeDirectory } = require('./helpers/utils')
 
 const TEST_DIRECTORY = path.join(os.tmpdir(), 'fs.copy')
+
 test.before(async () => {
-  await emptyDirectory(TEST_DIRECTORY)
+  await removeDirectory(TEST_DIRECTORY)
   await createDirectory(TEST_DIRECTORY)
 })
 
@@ -16,4 +18,12 @@ test.after(async () => {
   await removeDirectory(TEST_DIRECTORY)
 })
 
-test('should pass', t => t.pass())
+test('fs.copy', async t => {
+  const fileSrc = path.join(TEST_DIRECTORY, 'TEST_fs.copy')
+  const fileDest = path.join(TEST_DIRECTORY, 'TEST_fs.copy')
+  try {
+    await copy(fileSrc, fileDest)
+  } catch (err) {
+    t.truthy(err, 'should return an error if src and dest are the same')
+  }
+})
