6e4ea31e8760d899346c92307b90482ff7c9101d
Andrea Zucchini <azga84@gmail.com>
2018-02-21 21:21:05 +0100

    check the happy path

--- test/copy.test.js
+++ test/copy.test.js
@@ -3,10 +3,11 @@
 const test = require('ava')
 const os = require('os')
 const path = require('path')
-const { IsTheSameFileException } = require('../lib/IsTheSameFileException')
-const { copy } = require('../')
+const fs = require('fs')
+const { IsTheSameFileException } = require('../lib/errors/IsTheSameFileException')
+const { copy, pathExists } = require('../lib/copy')
 
-const { createDirectory, removeDirectory } = require('./helpers/utils')
+const { createDirectory, removeDirectory, createTestFile } = require('./helpers/utils')
 
 const TEST_DIRECTORY = path.join(os.tmpdir(), 'fs.copy')
 
@@ -23,14 +24,18 @@ test('fs.copy with the same source and destination', async t => {
   const fileSrc = path.join(TEST_DIRECTORY, 'TEST_fs.copy')
   const fileDest = path.join(TEST_DIRECTORY, 'TEST_fs.copy')
   const promise = copy(fileSrc, fileDest)
-  const err = await t.throws(promise, Error, 'should return an error if src and dest are the same', IsTheSameFileException)
+  const err = await t.throws(promise, IsTheSameFileException, 'should return an error if src and dest are the same')
   t.is(err.message, 'source and destination must not be the same')
 })
 
-test('fs.copy', async t => {
-  const fileSrc = path.join(TEST_DIRECTORY, 'TEST_fs.copy')
-  const fileDest = path.join(TEST_DIRECTORY, 'TEST_fs.copy')
+test('fs.copy correctly', async t => {
+  const fileSrc = path.join(TEST_DIRECTORY, 'TEST_fs.copySrc')
+  const fileDest = path.join(TEST_DIRECTORY, 'TEST_fs.copyDest')
+  const fileData = 'hello-world'
+  createTestFile(fileSrc, fileData)
   const promise = copy(fileSrc, fileDest)
-  const err = await t.throws(promise, Error, 'should return an error if src and dest are the same', IsTheSameFileException)
-  t.is(err.message, 'source and destination must not be the same')
+  await t.notThrows(promise)
+  const exist = await pathExists(fileDest)
+  t.true(exist, 'should exist the destination file')
+  t.is(fs.readFileSync(fileDest).toString(), fileData, 'should exist the destination file with correct content')
 })
