9ab8956f7284b798cf5d17a96207828e0fadfa3a
Andrea Zucchini <azga84@gmail.com>
2018-02-21 21:18:38 +0100

    rename file

--- test/copy.test.js
+++ test/copy.test.js
@@ -3,6 +3,7 @@
 const test = require('ava')
 const os = require('os')
 const path = require('path')
+const { IsTheSameFileException } = require('../lib/IsTheSameFileException')
 const { copy } = require('../')
 
 const { createDirectory, removeDirectory } = require('./helpers/utils')
@@ -18,10 +19,18 @@ test.after(async () => {
   await removeDirectory(TEST_DIRECTORY)
 })
 
+test('fs.copy with the same source and destination', async t => {
+  const fileSrc = path.join(TEST_DIRECTORY, 'TEST_fs.copy')
+  const fileDest = path.join(TEST_DIRECTORY, 'TEST_fs.copy')
+  const promise = copy(fileSrc, fileDest)
+  const err = await t.throws(promise, Error, 'should return an error if src and dest are the same', IsTheSameFileException)
+  t.is(err.message, 'source and destination must not be the same')
+})
+
 test('fs.copy', async t => {
   const fileSrc = path.join(TEST_DIRECTORY, 'TEST_fs.copy')
   const fileDest = path.join(TEST_DIRECTORY, 'TEST_fs.copy')
   const promise = copy(fileSrc, fileDest)
-  const err = await t.throws(promise, Error, 'should return an error if src and dest are the same')
+  const err = await t.throws(promise, Error, 'should return an error if src and dest are the same', IsTheSameFileException)
   t.is(err.message, 'source and destination must not be the same')
 })
