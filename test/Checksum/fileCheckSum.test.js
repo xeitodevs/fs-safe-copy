bb8d3b3c01604d1ba0a1d866aa4e4cb5bcfbdd9d
eloylp <eloy@sandboxwebs.com>
2018-02-26 23:22:43 +0100

    Created file checksum to easily compare files.

---
+++ test/Checksum/fileCheckSum.test.js
@@ -0,0 +1,35 @@
+const test = require('ava')
+const os = require('os')
+const path = require('path')
+
+const { createDirectory, removeDirectory, createTestFile } = require('../helpers/utils')
+const { fileEqual } = require('../../lib/Checksum/fileCheckSum')
+const TEST_DIRECTORY = path.join(os.tmpdir(), `fs.copy-${Date.now()}`)
+
+test.before(async () => {
+  await removeDirectory(TEST_DIRECTORY)
+  await createDirectory(TEST_DIRECTORY)
+})
+
+test.after(async () => {
+  await removeDirectory(TEST_DIRECTORY)
+})
+
+test('Checksum success on same file content', async t => {
+  const fileSrc = path.join(TEST_DIRECTORY, 'TEST_fs.checksumSameFileA')
+  const fileDest = path.join(TEST_DIRECTORY, 'TEST_fs.checksumSameFileB')
+  const fileData = 'hello-world'
+  createTestFile(fileSrc, fileData)
+  createTestFile(fileDest, fileData)
+  t.is(await fileEqual(fileSrc, fileDest), true)
+})
+
+test('Checksum must fail on different file content', async t => {
+  const fileSrc = path.join(TEST_DIRECTORY, 'TEST_fs.checksumDifferentFileA')
+  const fileDest = path.join(TEST_DIRECTORY, 'TEST_fs.checksumDifferentFileB')
+  const fileDataSrc = 'hello-world'
+  const fileDataDst = 'bye-world'
+  createTestFile(fileSrc, fileDataSrc)
+  createTestFile(fileDest, fileDataDst)
+  t.is(await fileEqual(fileSrc, fileDest), false)
+})
