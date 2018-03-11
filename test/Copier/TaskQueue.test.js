f8fdfef91801b99ae5f6cd922065ef32345a562c
Andrea Zucchini García <azga84@gmail.com>
2018-03-11 18:13:55 +0100

    improves the speed of the test

--- test/Copier/TaskQueue.test.js
+++ test/Copier/TaskQueue.test.js
@@ -9,7 +9,7 @@ test('TaskQueue', async t => {
 
   taskQueue.pushTasks([asyncTaskOne, asyncTaskTwo, asyncTaskThree])
   t.is(taskQueue.runningTask, 2, 'should exist only two running task')
-  await getPromise(2000)
+  await getPromise()
   t.is(taskQueue.runningTask, 1, 'should exist only two running task')
   await getPromise()
   t.is(taskQueue.runningTask, 0, 'there should be no task')
@@ -20,11 +20,11 @@ test('TaskQueue check result', async t => {
   const taskResult = {}
   taskQueue.pushTasks([getAsyncTaskWithCachedValues(taskResult)])
   t.is(taskQueue.runningTask, 1, 'should exist only two running task')
-  await getPromise(2000)
+  await getPromise(500)
   t.is(taskResult.value, 'cached value', 'should to have the correct value')
 })
 
-function getPromise (delay = 2000) {
+function getPromise (delay = 500) {
   return new Promise(resolve => {
     setTimeout(() => resolve(), delay)
   })
