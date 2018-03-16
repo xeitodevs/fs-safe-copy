dda8f4655ee37cffd88e4d3f5fd07834ed8e0e5e
Andrea Zucchini García <azga84@gmail.com>
2018-03-16 20:27:48 +0100

    check that the correct copy of the elements of a directory is done

--- test/Copier/FolderCopier.test.js
+++ test/Copier/FolderCopier.test.js
@@ -1,6 +1,8 @@
 const test = require('ava')
 const path = require('path')
-const { getTestDirectory } = require('../helpers/utils')
+const fs = require('fs')
+
+const { getTestDirectory, createTestFile } = require('../helpers/utils')
 const { RecursiveCopyException } = require('../../lib/errors/RecursiveCopyException')
 const { FolderCopier } = require('../../lib/Copier/FolderCopier')
 
@@ -27,4 +29,22 @@ test('copy subdirectory', async t => {
   const copier = new FolderCopier(fileSrc, fileDest)
   const exception = await t.throws(copier.copy())
   t.deepEqual(exception, new RecursiveCopyException(`cannot copy '${fileSrc}' to a subdirectory of itself, '${fileDest}'.`))
+})
+
+test('copy directory', async t => {
+  const directorySrc = getPath('directorySrc')
+  await createDirectory(directorySrc)
+  const dataInFile = 'data in file'
+  const numbers = Array.from({ length: 5 }, (val, index) => index)
+  for (const number of numbers) {
+    const fileName = `TEST_FILE_${number}.txt`
+    await createTestFile(path.join(directorySrc, fileName), `${dataInFile} ${fileName}`)
+  }
+  const copier = new FolderCopier(directorySrc, TEST_DIRECTORY)
+  await copier.copy()
+
+  for (const number of numbers) {
+    const fileName = `TEST_FILE_${number}.txt`
+    t.is(fs.readFileSync(getPath(fileName)).toString(), `${dataInFile} ${fileName}`, `should exist the file ${fileName} with correct content`)
+  }
 })
