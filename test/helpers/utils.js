3bbe250a8206c54481fa1ec4cf1db3b63fd6224b
eloylp <eloy@sandboxwebs.com>
2018-03-18 15:35:56 +0100

    Added utils for new tests.

--- test/helpers/utils.js
+++ test/helpers/utils.js
@@ -6,6 +6,7 @@ const path = require('path')
 const fs = require('fs')
 const { promisify } = require('util')
 const mkdir = promisify(fs.mkdir)
+const access = promisify(fs.access)
 
 function remove (directoryPath) {
   return new Promise((resolve, reject) => {
@@ -49,10 +50,44 @@ function getTestDirectory () {
   return path.join(os.tmpdir(), `fs.copy-${Date.now()}`)
 }
 
+function exists (path) {
+  return access(path)
+}
+
+class Temporizer {
+  constructor (maxTime) {
+    this._maxTime = maxTime
+    this._startTime = null
+    this._endTime = null
+  }
+
+  startCount () {
+    this._startTime = new Date()
+  }
+
+  endCount () {
+    this._endTime = new Date()
+  }
+
+  getDuration () {
+    return this._endTime.getTime() - this._startTime.getTime()
+  }
+
+  isPassing () {
+    return this.getDuration() <= this._maxTime
+  }
+
+  getMessage () {
+    return `Operation was expected to run in ${this._maxTime}, but it ran for ${this.getDuration()}`
+  }
+}
+
 module.exports = {
   removeDirectory,
   emptyDirectory,
+  exists,
   createDirectory,
   createTestFile,
-  getTestDirectory
+  getTestDirectory,
+  Temporizer
 }
